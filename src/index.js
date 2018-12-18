96d1d448fe64419adc46ad1ce830fe5d8e29559b
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2018-12-18 23:42:14 +0300

    Redux is added and implemented
    * redux, redux-thunk, react-redux packages are added.
    * redux-devtools-extension package is added for debugging on browser.
    * state/store folder is created.
    * Redux store is implemented(ReduxStore.js).
    * Root reducer is implemented(rootReducer.js).
    * index.js is updated according to implementation of Redux.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ReduxStore from './state/store/ReduxStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ReduxStore.store}>
+    <App /> 
+  </Provider>,
+  document.getElementById('root')
+);
