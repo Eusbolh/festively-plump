8fff015478f51214a7c0292a81abb856fe10587b
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2018-12-22 18:28:44 +0300

    Carbon Design System is implemented.
    * carbon-components, carbon-components-react and carbon-icons packages are added.
    * A demo for Carbon Design System usage is added.

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Accordion, AccordionItem, Breadcrumb, BreadcrumbItem } from 'carbon-components-react';
+import 'carbon-components/scss/globals/scss/styles.scss';
 
-const Home = () => (
-  <div>
-    Hello, stranger!
-  </div>
-)
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home-demo">
+        <h3 className="App__demo-title">Carbon Components</h3>
+        <Accordion className="accordion">
+          <AccordionItem title="Example">
+            <p>
+              This is a Component imported from Carbon and styled with the CSS
+              from the main Carbon Components GitHub repo!
+            </p>
+          </AccordionItem>
+          <AccordionItem title="Questions?">
+            <p>
+              Hi there!{' '}
+              <span aria-label="Hand wave" role="img">
+                👋{' '}
+              </span>{' '}
+              if you have any questions about this demo, or are running into
+              any issues setting this up in your own development environment,
+              please feel free to reach out to us on Slack or make an issue on
+              the GitHub Repository.
+            </p>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    );
+  }
+}
 
 export default Home;
